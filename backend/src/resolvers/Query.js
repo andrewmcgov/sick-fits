87b91349a74064292a6f51a24957cb34b8d96151
andrewmcgov <andrew.mcgoveran@shopify.com>
2018-11-07 20:23:14 -0500

    Added resolver queries and mutations

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,10 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  // anytime a query is the exact same on in both Yoga and Prisma
+  // You can forward it right to Prisma using forwardTo
+  // Instead of writing it all out - great for queries that need no auth, etc
+  items: forwardTo("db")
+};
 
 module.exports = Query;
